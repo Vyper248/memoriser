3c8fa2a28cca21a3dfe8b9b659023c5573d79fd4
Christopher T Wilson <vyper375@hotmail.com>
2023-03-16 17:18:59 +0000

    Added more instructional cards to initial state.

--- src/redux/mainSlice.ts
+++ src/redux/mainSlice.ts
@@ -20,10 +20,24 @@ export type MainSlice = {
 export const initialState: MainSlice = {
     cards: [
         {
-            id: '1',
+            id: '5',
             groupId: '1',
-            question: 'Look at a card, try to guess, then check if you got it right by clicking on it.',
-            answer: 'Be truthful here, otherwise it won\'t help you.',
+            question: 'Click on the question mark icon next to the heading for more details.',
+            answer: 'I hope this helps with your learning!',
+            points: 0,
+        },       
+        {
+            id: '4',
+            groupId: '1',
+            question: 'You can use the filter menu above to only view specific colours.',
+            answer: 'This will remain active when changing groups. Use the Clear Filter button to reset it.',
+            points: 0,
+        },
+        {
+            id: '3',
+            groupId: '1',
+            question: 'You can use the share menu above to share a link or backup/import data.',
+            answer: 'When you click on one of the Share buttons, a link will be copied to your clipboard for sharing.',
             points: 0,
         },
         {
@@ -32,7 +46,15 @@ export const initialState: MainSlice = {
             question: 'You can use the dropdown menu above to add, edit or delete a group.',
             answer: 'So start creating some cards and learning!',
             points: 0,
-        }
+        },
+        {
+            id: '1',
+            groupId: '1',
+            question: 'Look at a card, try to guess, then check if you got it right by clicking on it.',
+            answer: 'Be truthful here, otherwise it won\'t help you.',
+            points: 0,
+        },
+        
     ],
     groups: [
         {
